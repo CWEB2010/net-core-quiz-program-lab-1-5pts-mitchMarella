db9871a91bb6940af1213a7cc50c8895a8351b4b
Marella <marmita@dunwoody.edu>
2019-01-18 19:53:43 -0600

    Final Commit.

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -15,6 +15,9 @@ namespace lab_one
 				"5. When was .Net Core 3.0 Announced?",
 				"6. Which Language does .Net Core not have support for?",
 				"7. What is the current .Net Core version?",
+				"8. Which one of these is .Net Core not used for?",
+				"9. What is the .Net github repository?",
+				"10. Which of these packages is not included with .Net Core?"
 			};
 
 			string[,] quizAnswers = new string[10, 4]{
@@ -25,9 +28,9 @@ namespace lab_one
 				{"A) 2017", "B) 2015", "C) 2013", "D) 2018"},
 				{"A) Ruby", "B) C#", "C) F#", "D) VB .NET"},
 				{"A) 2.2", "B) 2.0", "C) 2.0.2", "D) 2.3.1"},
-				{"A) ", "B) ", "C) Microsoft", "D) "},
-				{"A) ", "B) ", "C) ", "D) yes"},
-				{"A) yes ", "B) ", "C) ", "D) "}
+				{"A) Building Websites", "B) Creating Services", "C) Hosting Websites", "D) Building Console Apps"},
+				{"A) github.com/core", "B) github.com/core/dotnet", "C) github.com/dotnet", "D) github.com/dotnet/core"},
+				{"A) Razor", "B) CoreCLR", "C) CoreRT", "D) CoreFX"}
 				};
 
 			string EXIT = "";
